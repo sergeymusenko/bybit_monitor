e0d75b6027c788509c1b51666cc5cdc7be6d9d91
sergeymusenko <musenko@gmail.com>
2024-01-17 23:02:38 +0200

    init

---
+++ config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""\
+Config file fo Trading Bot
+-- PLEAS EKEEP IT SECURE --
+"""
+
+__project__	= "Trading Bot - pybit"
+__author__	= "Sergey V Musenko"
+__email__	= "sergey@musenko.com"
+__copyright__= "© 2023, musenko.com"
+__license__	= "GPL"
+__credits__	= ["Sergey Musenko"]
+__date__	= "2024-01-10"
+__version__	= "0.1"
+__status__	= "dev"
+
+#api_net = 'https://api-testnet.bybit.com'
+api_net = 'https://api.bybit.com'
+api_key = 'key'
+api_secret = 'secret'
+# Bybit config -- PLEAS EKEEP IT SECURE --
